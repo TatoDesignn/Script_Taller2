8f1d225b63cb9c7bcfa66c4cbd70eb8c2af18374
TATO DESIGN <santi1404@hotmail.com>
2024-04-03 22:44:53 -0500

    Delegado con parametros ejercicios

---
+++ DelegadoParametros/Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+public delegate int CalculadoraDel(int a , int b);
+
+public class Calculadora
+{
+    public int Sumar(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Restar(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int Multiplicar(int a, int b)
+    {
+        return a * b;
+    }
+
+    public int Dividir(int a, int b)
+    {
+        return a / b;
+    }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculadora calc = new Calculadora();
+
+        CalculadoraDel resultado = calc.Sumar;
+        Console.WriteLine(resultado(1, 1));
+
+        resultado = calc.Restar;
+        Console.WriteLine(resultado(2, 1));
+
+        resultado = calc.Multiplicar;
+        Console.WriteLine(resultado(2, 2)); 
+
+        resultado = calc.Dividir;
+        Console.WriteLine(resultado(4, 2));
+    }
+}
