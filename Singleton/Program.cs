562aac6099193311664c706078f4110765e34848
TATO DESIGN <santi1404@hotmail.com>
2024-04-06 22:28:28 -0500

    Ejercicio singleton en C#

---
+++ Singleton/Program.cs
@@ -0,0 +1,27 @@
+﻿
+public class ClaseSingleton
+{
+    public static readonly ClaseSingleton instance = new ClaseSingleton();
+
+    private ClaseSingleton() { }
+
+    public static ClaseSingleton ObtenerInstancia()
+    {
+        return instance;
+    }
+
+    public void Saludar()
+    {
+        Console.WriteLine("Hola mundo.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ClaseSingleton singleton = ClaseSingleton.ObtenerInstancia();
+
+        singleton.Saludar();
+    }
+}
